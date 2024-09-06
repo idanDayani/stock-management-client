4a5d164e0f4e11b942fd21cbab69b5b5b668d7d7
IdanDayani8 <idand@yuvital.com>
2024-09-06 11:42:18 +0300

    refactor: Update typography styles in BackButton and StockTitle components

--- src/pages/stockQuote/components/backButton.tsx
+++ src/pages/stockQuote/components/backButton.tsx
@@ -6,7 +6,11 @@ export function BackButton() {
 
   return (
     <div className="absolute top-4 right-4">
-      <Button onClick={() => navigate("/")} type="primary">
+      <Button
+        onClick={() => navigate("/")}
+        type="primary"
+        className="bg-gradient-to-r from-yellow-400 to-orange-500 border-none font-semibold py-2 px-4 rounded-lg"
+      >
         Back to Portfolio
       </Button>
     </div>
--- src/pages/stockQuote/components/stockTitle.tsx
+++ src/pages/stockQuote/components/stockTitle.tsx
@@ -4,7 +4,7 @@ import Text from "antd/es/typography/Text";
 const StockTitle = observer((props: { name?: string; symbol?: string }) => {
   const { name, symbol } = props;
   return (
-    <Text className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-emerald-400 mb-5">
+    <Text className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-500 mb-5">
       {name} {symbol}
     </Text>
   );
